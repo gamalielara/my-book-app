12fdfca0f26c69919b7954b2c8ad9ba12ce34bc1
gamalielara <aragamalielboanerges@gmail.com>
2024-11-27 10:15:26 +0700

    add fantasy and nonfict fragment

--- app/src/main/java/com/example/mybookapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/mybookapp/ui/gallery/GalleryFragment.kt
@@ -5,5 +5,5 @@ import com.example.mybookapp.data.booksListMockData
 import com.example.mybookapp.ui.bookListBaseFragment.BookListBaseFragment
 
 class GalleryFragment : BookListBaseFragment(
-    title = BookGenre.FANTASY,
-    bookList = booksListMockData.filter { it.genre == BookGenre.FANTASY })
+    title = BookGenre.SCIENCE_FICTION,
+    bookList = booksListMockData.filter { it.genre == BookGenre.SCIENCE_FICTION })
--- app/src/main/java/com/example/mybookapp/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/mybookapp/ui/slideshow/SlideshowFragment.kt
@@ -1,42 +1,9 @@
 package com.example.mybookapp.ui.slideshow
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.mybookapp.databinding.FragmentSlideshowBinding
+import com.example.mybookapp.data.BookGenre
+import com.example.mybookapp.data.booksListMockData
+import com.example.mybookapp.ui.bookListBaseFragment.BookListBaseFragment
 
-class SlideshowFragment : Fragment() {
-
-    private var _binding: FragmentSlideshowBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
-
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+class SlideshowFragment : BookListBaseFragment(
+    title = BookGenre.NONFICTION,
+    bookList = booksListMockData.filter { it.genre == BookGenre.NONFICTION })
