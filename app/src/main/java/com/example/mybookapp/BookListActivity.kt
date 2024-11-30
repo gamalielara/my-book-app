83bfe7784a96d819a80a8abfdee2c34106f89cbf
gamalielara <aragamalielboanerges@gmail.com>
2024-11-30 15:42:21 +0700

    add poetry and general fiction redirection

--- app/src/main/java/com/example/mybookapp/BookListActivity.kt
+++ app/src/main/java/com/example/mybookapp/BookListActivity.kt
@@ -2,16 +2,16 @@ package com.example.mybookapp
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.example.mybookapp.databinding.ActivityBookListBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class BookListActivity : AppCompatActivity() {
 
@@ -34,11 +34,14 @@ class BookListActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_book_list)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_fantasy, R.id.nav_scifi, R.id.nav_nonfiction
+                R.id.nav_fantasy,
+                R.id.nav_scifi,
+                R.id.nav_nonfiction,
+                R.id.nav_poetry,
+                R.id.nav_general_fiction
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -46,7 +49,6 @@ class BookListActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.book_list, menu)
         return true
     }
