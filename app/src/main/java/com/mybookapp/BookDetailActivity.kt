019f301fb7d16cc8cccb29338a3e01c7bff3906d
gamalielara <aragamalielboanerges@gmail.com>
2024-12-04 08:16:14 +0700

    explicitly set status bar color

--- app/src/main/java/com/mybookapp/BookDetailActivity.kt
+++ app/src/main/java/com/mybookapp/BookDetailActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mybookapp.data.BookListIntentKeys
@@ -14,6 +15,7 @@ class BookDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_book_detail)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -21,8 +23,11 @@ class BookDetailActivity : AppCompatActivity() {
         }
 
         val binding = ActivityBookDetailBinding.inflate(layoutInflater)
-        
+
         setSupportActionBar(binding.appBarBookDetail.toolbar)
+
+        // Explicitly set the status bar color
+        window.statusBarColor = ContextCompat.getColor(this, R.color.md_theme_primary)
 
         val hehe = intent.getSerializableExtra(BookListIntentKeys.BOOK_DETAIL_DATA)
         Log.d("HAI ", hehe.toString())
